8cdb719d7f9ba2424c7c58d6a810f9b7ae7d47c1
rawanasser <155817992+rawanasser@users.noreply.github.com>
2024-05-10 20:10:18 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function redirectToPage() {
+    window.location.href = "https://saudi.pizzahut.me/"
+}
+
+function skills() {
+    window.location.href = "https://www.mrassem.com/2022/06/characteristics-painter.html"
+}
